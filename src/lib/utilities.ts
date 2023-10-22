9738a16dacc8bb0e466591224f2ec721a45b06b4
Brad Meyn <bradmeyn@Brads-MacBook-Air-2.local>
2023-10-22 19:17:57 +1100

    come back to this

---
+++ src/lib/utilities.ts
@@ -0,0 +1,17 @@
+export function formatCurrency(value: number) {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 0,
+		maximumFractionDigits: 0
+	}).format(value);
+}
+
+export const frequencyOptions = [
+	{ name: 'Weekly', value: 52 },
+	{ name: 'Fortnightly', value: 26 },
+	{ name: 'Monthly', value: 12 },
+	{ name: 'Quarterly', value: 4 },
+	{ name: 'Half Yearly', value: 2 },
+	{ name: 'Yearly', value: 1 }
+];

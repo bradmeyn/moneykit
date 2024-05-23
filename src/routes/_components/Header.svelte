05ad6b028df8a1951e376f92fc89a3a7a7f95ab6
bradmeyn <bradjmeyn@gmail.com>
2024-05-24 08:30:59 +1000

    add responsive header

--- src/routes/_components/Header.svelte
+++ src/routes/_components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Hamburger from './Hamburger.svelte';
 	import Navbar from './Navbar.svelte';
 	import Icon from '@iconify/svelte';
 
@@ -6,22 +7,10 @@
 	let isMobile = false;
 </script>
 
-<header class="z-20 py-4 border border-slate-600">
-	<div class="container flex justify-between items-center relative z-10">
-		<!-- <Navbar>
-			<NavBrand href="/">
-				<Icon icon="streamline:bag-dollar-solid" class="w-6 h-6 text-emerald-500 mr-2" />
-				<span class="text-white font-bold text-xl">WealthKit</span>
-			</NavBrand>
-			<NavHamburger />
-			<NavUl {activeUrl}>
-				<NavLi href="/growth-calculator">Growth Calculator</NavLi>
-				<NavLi href="/portfolio-builder">Portfolio Builder</NavLi>
-				<NavLi href="/personal-tax-calculator">Tax Calculator</NavLi>
-			</NavUl>
-		</Navbar> -->
-		<a href="/" class="flex items-center">
-			<Icon icon="streamline:bag-dollar-solid" class="w-6 h-6 text-emerald-500 mr-2" />
+<header class="z-50 h-16 bg-slate-900 lg:flex border">
+	<div class="container border h-full flex justify-between items-center relative z-10">
+		<a href="/" class="flex items-center gap-2">
+			<Icon icon="streamline:bag-dollar-solid" class="w-6 h-6 text-emerald-500 " />
 			<span class="text-white font-semibold text-xl"
 				>Wealth<span
 					class="text-transparent bg-clip-text bg-gradient-to-b from-emerald-300 to-emerald-500"
@@ -30,16 +19,9 @@
 			>
 		</a>
 
-		<Navbar {isOpen} />
-		<button
-			class="block md:hidden text-white"
-			on:click={() => {
-				isOpen = !isOpen;
-			}}
-		>
-			<Icon icon="carbon:menu" class="w-6 h-6" />
-		</button>
+		<Hamburger bind:isOpen />
 	</div>
+	<Navbar {isOpen} />
 </header>
 {#if isOpen}
 	<div class="fixed inset-0 bg-black opacity-50 z-10" />
--- src/routes/_components/Navbar.svelte
+++ src/routes/_components/Navbar.svelte
@@ -11,15 +11,15 @@
 </script>
 
 {#if isOpen}
-	<nav class="absolute lg:static z-10 w-full bg-slate-700 top-20 border border-red-600">
-		<ul class="w-full">
+	<nav class="absolute lg:static z-10 w-full border bg-slate-900 top-16 py-4">
+		<ul class="w-full lg:w-fit lg:flex">
 			{#each links as link}
 				<li>
 					<a
 						href={link.href}
-						class="w-full p-4 block {activeUrl === link.href
+						class="px-4 block border-slate-700 {activeUrl === link.href
 							? 'text-white'
-							: 'text-slate-500 hover:text-white'}"
+							: 'text-slate-300 hover:text-white hover:bg-slate-600'}"
 						>{link.name}
 					</a>
 				</li>
